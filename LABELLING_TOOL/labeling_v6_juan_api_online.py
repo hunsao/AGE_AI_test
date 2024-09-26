903777146459189e8f755a55c3a63ddf8f24ae84
David C. <84016055+hunsao@users.noreply.github.com>
2024-09-26 14:30:49 +0200

    Update labeling_v6_juan_api_online.py

--- LABELLING_TOOL/labeling_v6_juan_api_online.py
+++ LABELLING_TOOL/labeling_v6_juan_api_online.py
@@ -67,7 +67,7 @@ class ExponentialBackoffHttpRequest(HttpRequest):
 @retry(
     stop=stop_after_attempt(3),
     wait=wait_exponential(multiplier=1, min=4, max=10),
-    retry=retry_if_exception_type((socket.timeout, TimeoutError))
+    retry=retry_if_exception_type((socket.timeout, TimeoutError)))
 
 def get_google_services():
     try:
